bef4679746dc07f43b28a8c3433b4e9c75b3ffc6
Mark <msorial44@gmail.com>
2021-04-11 13:06:11 +0400

    added comments and working reset button

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import fired from './Images/fired.svg';
 import target from './Images/target.svg';
 import './App.css';
 
-function CompFired() {
+function CompFired() { //component to house image
   return <img className='cell-img' src={fired} alt="" />
 }
 
-function CompTarget() {
+function CompTarget() { //component to house image
   return <img className='cell-img' src={target} alt="" />
 }
 
-function randomInt(min, max) { // min and max included 
+function randomInt(min, max) { // random integer for bomb, min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-let bombList = [];
-for (let i = 1; i <= 10; i++) {
+let bombList = []; //list of bombs
+for (let i = 1; i <= 10; i++) { //get random tiles to become bombs
   var randCol = randomInt(1, 8);
   var randRow = randomInt(1, 9);
   if (bombList.includes([randRow, randCol])) {
@@ -28,14 +28,14 @@ for (let i = 1; i <= 10; i++) {
   }
 } 
 
-let zeroList = [];
-let targetCounterVar = 10;
+let zeroList = []; //list of tiles that have no bombs near it
+let targetCounterVar = 10; //var to count how many flags left
 
-class Cell extends React.Component {
+class Cell extends React.Component { //main cell code
   constructor(props) {
     super(props);
     this.state = {
-      val: 'closed', 
+      val: 'closed', //has the tile been opened or not
       isBomb: false, 
       isFlagged: false, 
       col: 0, 
@@ -46,7 +46,7 @@ class Cell extends React.Component {
 
   componentDidMount() {
     this.bombSetup();
-    this.interval = setInterval(() => this.revealTiles(), 100);
+    this.interval = setInterval(() => this.revealTiles(), 100); //set timer to check for opened zero tiles
   }
 
   updateState = () => {
@@ -59,72 +59,72 @@ class Cell extends React.Component {
       })
   };
 
-  componentWillUnmount() {
+  componentWillUnmount() { //stop timer and right click menu
     document.removeEventListener("contextmenu", this.handleRightClick);
     clearInterval(this.interval);
   }
 
-  handleClick() {
-    if (this.state.isFlagged){
+  handleClick() { //function to handle clicks
+    if (this.state.isFlagged){ //if player opens a tile with a target removes target and adds to total targets
       targetCounterVar++;
     }
-    this.setState(state => ({val: 'open'}));
-    this.addZero();
+    this.setState(state => ({val: 'open'})); //opens tile
+    this.addZero();//if tile is a 0 add to zerolist
   }
 
   
 
-  handleRightClick = (e) => {
+  handleRightClick = (e) => { //right click handler
     e.preventDefault();
-    if (this.state.val === 'closed') {
-      if (this.state.isFlagged){
+    if (this.state.val === 'closed') { //check if tile is still closed
+      if (this.state.isFlagged){ //unflag a tile
         this.setState(state => ({isFlagged: false}));
         targetCounterVar++;
-      }else{
+      }else{ //flag a tile
         this.setState(state => ({isFlagged: true}));
         targetCounterVar--;
       }
     }
   }
 
-  addZero() {
+  addZero() { //add to zerolist if no bombs next to it
     if (this.state.bombCounter === 0) {
       zeroList.push([this.props.row, this.props.col]);
     }
   } 
 
-  revealTiles() {
+  revealTiles() { //handle reavling neighbor zeros
     if (this.state.val === 'closed') {
       for (const x of zeroList) {
-        if (x[0] === this.props.row && (x[1] - 1) === this.props.col) {
+        if (x[0] === this.props.row && (x[1] - 1) === this.props.col) { //checks if a zero is directly above
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if (x[0] === this.props.row && (x[1] + 1) === this.props.col) {
+        if (x[0] === this.props.row && (x[1] + 1) === this.props.col) {//checks if a zero is directly below
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]-1) === this.props.row && x[1] === this.props.col) {
+        if ((x[0]-1) === this.props.row && x[1] === this.props.col) { //checks if a zero is directly left
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]+1) === this.props.row && x[1] === this.props.col) {
+        if ((x[0]+1) === this.props.row && x[1] === this.props.col) { //checks if a zero is directly right
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]-1) === this.props.row && (x[1]-1) === this.props.col) {
+        if ((x[0]-1) === this.props.row && (x[1]-1) === this.props.col) { //checks if a zero is to the top left
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]-1) === this.props.row && (x[1]+1) === this.props.col) {
+        if ((x[0]-1) === this.props.row && (x[1]+1) === this.props.col) { //checks if a zero is to the bottom left
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]+1) === this.props.row && (x[1]-1) === this.props.col) {
+        if ((x[0]+1) === this.props.row && (x[1]-1) === this.props.col) { //checks if a zero is to the top right
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
-        if ((x[0]+1) === this.props.row && (x[1]+1) === this.props.col) {
+        if ((x[0]+1) === this.props.row && (x[1]+1) === this.props.col) {//checks if a zero is to the bottom right
           this.setState(state => ({val: 'open'}));
           this.addZero();
         };
@@ -132,16 +132,16 @@ class Cell extends React.Component {
     }
   }
 
-  updateId() {
+  updateId() { //adds passed props to state
     this.setState(state => ({row: this.props.row}));
     this.setState(state => ({col: this.props.col}));
   }
 
-  bombAdd() {
+  bombAdd() { //if called adds to bombCounter which displays amount of bombs adjacent
     this.setState(prevState => ({bombCounter: prevState.bombCounter + 1}));
   }
 
-  bombSetup() {
+  bombSetup() { //checks current tile if its a bomb
     for (const x of bombList) {
       if (x[0] === this.props.row && x[1] === this.props.col) {
         this.setState(state => ({bombCounter: 99}));
@@ -151,28 +151,28 @@ class Cell extends React.Component {
     };
     
     for (const x of bombList) {
-      if (x[0] === this.props.row && (x[1] - 1) === this.props.col) {
+      if (x[0] === this.props.row && (x[1] - 1) === this.props.col) { //checks for bomb in top tile
         this.bombAdd();
       };
-      if (x[0] === this.props.row && (x[1] + 1) === this.props.col) {
+      if (x[0] === this.props.row && (x[1] + 1) === this.props.col) { //checks for bomb in bottom tile
         this.bombAdd();
       };
-      if ((x[0]-1) === this.props.row && x[1] === this.props.col) {
+      if ((x[0]-1) === this.props.row && x[1] === this.props.col) { //checks for bomb in left tile
         this.bombAdd();
       };
-      if ((x[0]+1) === this.props.row && x[1] === this.props.col) {
+      if ((x[0]+1) === this.props.row && x[1] === this.props.col) { //checks for bomb in right tile
         this.bombAdd();
       };
-      if ((x[0]-1) === this.props.row && (x[1]-1) === this.props.col) {
+      if ((x[0]-1) === this.props.row && (x[1]-1) === this.props.col) { //checks for bomb in top left tile
         this.bombAdd();
       };
-      if ((x[0]-1) === this.props.row && (x[1]+1) === this.props.col) {
+      if ((x[0]-1) === this.props.row && (x[1]+1) === this.props.col) { //checks for bomb in bottom left tile
         this.bombAdd();
       };
-      if ((x[0]+1) === this.props.row && (x[1]-1) === this.props.col) {
+      if ((x[0]+1) === this.props.row && (x[1]-1) === this.props.col) { //checks for bomb in top right tile
         this.bombAdd();
       };
-      if ((x[0]+1) === this.props.row && (x[1]+1) === this.props.col) {
+      if ((x[0]+1) === this.props.row && (x[1]+1) === this.props.col) {//checks for bomb in bottom right tile
         this.bombAdd();
       };
     };
@@ -181,7 +181,7 @@ class Cell extends React.Component {
   render() { 
     let img;
     let clsName;
-    if (this.state.val === 'closed') {
+    if (this.state.val === 'closed') { //displays tile if revealed, closed, flagged, or bomb
       if (this.state.isFlagged) {
         img = <CompTarget />
         clsName = 'cell-closed';
@@ -207,7 +207,7 @@ class Cell extends React.Component {
   }
 }
 
-class TargetCounter extends React.Component {
+class TargetCounter extends React.Component { //counter component for counting flagged tiles
   constructor(props) {
     super(props);
     this.state = {  
@@ -215,15 +215,27 @@ class TargetCounter extends React.Component {
     }
   }
   updateCounter() {
-    this.setState(state => ({num: targetCounterVar}));
+    this.setState(state => ({num: targetCounterVar})); //changes state to current flags left
   }
   componentDidMount() {
-    this.interval = setInterval(() => this.updateCounter(), 100);
+    this.interval = setInterval(() => this.updateCounter(), 100); //time to keep checking flags
   }
 
   render() { 
     return (  
      <p>{this.state.num}</p>
+    );
+  }
+}
+ 
+class ResetButton extends React.Component { //reset button
+  handleClick() {
+    window.location.reload(); //reloads page
+  }
+
+  render() { 
+    return (  
+      <button onClick={this.handleClick}>Reset</button>
     );
   }
 }
@@ -235,7 +247,7 @@ class App extends React.Component {
     this.state = {uselessUpdate: 0};
   }
 
-  nestedArray = [
+  nestedArray = [ //nested array for all tiles
     [<Cell row={1} col={1} />, <Cell row={1} col={2} />, <Cell row={1} col={3} />, <Cell row={1} col={4} />, <Cell row={1} col={5} />, <Cell row={1} col={6} />, <Cell row={1} col={7} />, <Cell row={1} col={8} />],
     [<Cell row={2} col={1} />, <Cell row={2} col={2} />, <Cell row={2} col={3} />, <Cell row={2} col={4} />, <Cell row={2} col={5} />, <Cell row={2} col={6} />, <Cell row={2} col={7} />, <Cell row={2} col={8} />],
     [<Cell row={3} col={1} />, <Cell row={3} col={2} />, <Cell row={3} col={3} />, <Cell row={3} col={4} />, <Cell row={3} col={5} />, <Cell row={3} col={6} />, <Cell row={3} col={7} />, <Cell row={3} col={8} />],
@@ -247,13 +259,13 @@ class App extends React.Component {
     [<Cell row={9} col={1} />, <Cell row={9} col={2} />, <Cell row={9} col={3} />, <Cell row={9} col={4} />, <Cell row={9} col={5} />, <Cell row={9} col={6} />, <Cell row={9} col={7} />, <Cell row={9} col={8} />]
   ]
 
-  uselessUpdateFunction() {
+  uselessUpdateFunction() { //dont ask
     this.setState(prevState => ({uselessUpdate: prevState.uselessUpdate + 1}));
   }
 
   
 
-  render() {
+  render() { //page heirarchy
     return (
       <div className="App">
         <div className="App-container">
@@ -266,7 +278,7 @@ class App extends React.Component {
               <TargetCounter var={targetCounterVar} func={this.uselessUpdateFunction}/>
             </div>
             <div className="reset">
-              <button>Reset</button>
+              <ResetButton />
             </div>
             <div className="timer">
               <p>0:00</p>
