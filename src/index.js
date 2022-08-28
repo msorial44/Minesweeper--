dd35290c63e5a9492d2613b3a92d16f8f8cab043
Mark Sorial <65821211+msorial44@users.noreply.github.com>
2022-08-28 05:17:31 +0000

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 
 ReactDOM.render(
   <React.StrictMode>
